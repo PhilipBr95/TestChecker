04dd2c493fdbeb25117c92ec94a2f993566efa68
Phil <PB@Bettingaid.com>
2026-03-15 00:51:30 +0000

    Owin bugs

--- src/TestChecker.Runner/Owin/TestEndpointExtensions.cs
+++ src/TestChecker.Runner/Owin/TestEndpointExtensions.cs
@@ -13,7 +13,7 @@ namespace TestChecker.Runner
 {
     public static partial class TestEndpointExtensions
     {
-        public static void UseTestEndpoint<TData>(this IAppBuilder app, Assembly assembly, List<ITestCheckDependency> dependencies, Func<ITestChecks<TData>> testChecks, ILoggerFactory loggerFactory, IMethodNameExtractorService methodNameExtractorService = null, string readEnvironmentName = READ_ENVIRONMENT_NAME, string readWriteEnvironmentName = READ_WRITE_ENVIRONMENT_NAME) where TData : class
+        public static void UseTestEndpoint<TData>(this IAppBuilder app, Assembly assembly, List<ITestCheckDependency> dependencies, Func<ITestChecks<TData>> testChecks, ILoggerFactory loggerFactory, IMethodNameExtractorService methodNameExtractorService = null, string readEnvironmentName = READ_ENVIRONMENT_NAME, string readWriteEnvironmentName = READ_WRITE_ENVIRONMENT_NAME) where TData : class, new()
         {
             _methodNameExtractor = methodNameExtractorService ?? new MethodNameExtractorService();
             _testCheckDependencyRunner = new TestCheckDependencyRunner(dependencies, loggerFactory.CreateLogger<TestCheckDependencyRunner>());
@@ -25,7 +25,15 @@ namespace TestChecker.Runner
 
                 app.Use(async (context, next) =>
                 {
-                    if (await HandleContextAsync(runner, testChecks, context) == false)
+                    if (IsTestRequest(context.Request.Path.Value))
+                    {
+                        var settings = await TestSettingsRetriever.GetSettingsAsync(context.Request).ConfigureAwait(false);
+                        var response = await HandleContextAsync(context.Request.Path.Value, context.Request.Uri.ToString(), settings, runner, testChecks);
+
+                        context.Response.ContentType = response.ContentType;
+                        await context.Response.WriteAsync(response.Content).ConfigureAwait(false);
+                    }
+                    else
                     {
                         await next().ConfigureAwait(false);
                     }
--- src/TestChecker.Runner/Owin/TestSettingsRetriever.cs
+++ src/TestChecker.Runner/Owin/TestSettingsRetriever.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using TestChecker.Core;
+using System.Linq;
 
 namespace TestChecker.Runner
 {
@@ -15,24 +16,26 @@ namespace TestChecker.Runner
         {
             var action = request.Query["Action"];
             var apiKey = request.Query["ApiKey"];
-            var useUIString = request.Query["UseUI"];
-            bool.TryParse(useUIString, out bool useUI);
+            var useUIString = request.Query["UseUI"];            
 
             var path = GetTestEndPoint(request.PathBase.Value, request.Path.Value);
 
             var formData = await request.ReadFormAsync().ConfigureAwait(false);
 
-            var testMethodsData = formData.Get("TestMethods");
-            var testMethods = string.IsNullOrWhiteSpace(testMethodsData) ? default : testMethodsData.Split(',');
+            var testMethods = formData.GetValues("TestMethods");
             var testDataJson = formData.Get("TestData");
-
+            
             if (string.IsNullOrWhiteSpace(apiKey))
                 apiKey = formData.Get("ApiKey");
 
             if (string.IsNullOrWhiteSpace(action))
                 action = formData.Get("Action");
 
-            return new TestSettings(path, apiKey, testDataJson, GetAction(action, request.Path.Value), testMethods) { UseUI = useUI };
+            if (string.IsNullOrWhiteSpace(useUIString))
+                useUIString = formData.Get("UseUI");
+
+            bool.TryParse(useUIString, out useUI);
+            return new TestSettings(path, apiKey, testDataJson, GetAction(action, request.Path.Value), testMethods?.ToArray()) { UseUI = useUI };
         }
     }
 }
